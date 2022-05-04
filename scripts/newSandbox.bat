77873d0ba75af4eb2dc704bbe123e6103dc42aaa
Carl <carl.fosli@gmail.com>
2022-05-04 22:10:33 +0200

    Update newSandbox.bat

--- scripts/newSandbox.bat
+++ scripts/newSandbox.bat
@@ -1,5 +1,5 @@
-echo "Oppretter scratch org"
-call sfdx force:org:create --type sandbox --definitionfile config/dev-sandbox-def.json -a %1 -s -w 120 -u NAV_PROD_SFDX
+echo "Oppretter Sandbox"
+call sfdx force:org:create --type sandbox --definitionfile config/dev-sandbox-def.json -a %1 -s -w 240 -u NAV_PROD_SFDX
 
 echo "Ferdig"
 
